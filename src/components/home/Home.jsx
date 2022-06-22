edefdcd02a0f7115a18b83c1a559338b3e6e583f
Roche001 <rocheharold13@gmail.com>
2022-06-22 12:28:17 +0300

    update changes

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,10 +2,16 @@ import React from "react";
 import "./Home.css";
 const Home = () => {
   return (
-    <div id="Home" className="home">
-      <div className="home-header">
-        <h2>Take Action!</h2>
-        <h5>Stop the Violence.</h5>
+    <div id="Home">
+      <div className="home">
+        <div className="home-header">
+          <h2>Take Action!</h2>
+          <h5>Stop the Violence.</h5>
+        </div>
+      </div>
+
+      <div className="text-head">
+        <h4>The Consequences of Violence on Community Mental Health</h4>
       </div>
     </div>
   );
