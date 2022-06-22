dffe2d0738f4a63f827f904026d00bf88469d553
Roche001 <rocheharold13@gmail.com>
2022-06-22 14:25:48 +0300

    update changes

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,26 @@
 import React from "react";
 import "./Footer.css";
+import { BsFacebook } from "react-icons/bs";
+import { AiFillInstagram } from "react-icons/ai";
+import { BsTwitter } from "react-icons/bs";
+import { BsLinkedin } from "react-icons/bs";
 const Footer = () => {
   return (
     <div id="footer">
       <div className="icons">
-        <a href="https://www.facebook.com/"></a>
+        <a href="https://www.facebook.com/">
+          {" "}
+          <BsFacebook />
+        </a>
+        <a className="icon icon-instagram" href="https://www.instagram.com">
+          <AiFillInstagram />
+        </a>
+        <a href="https://www.twitter.com">
+          <BsTwitter />
+        </a>
+        <a href="https://www.linkedin.com">
+          <BsLinkedin />
+        </a>
       </div>
     </div>
   );
