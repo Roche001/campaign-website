dce376606f0c559dab568187bff10b65a1ba752a
Roche001 <rocheharold13@gmail.com>
2022-06-22 11:34:55 +0300

    update changes

--- src/components/top/Top.jsx
+++ src/components/top/Top.jsx
@@ -1,8 +1,22 @@
 import React from "react";
 import "./Top.css";
+import { Container, Nav, Navbar } from "react-bootstrap";
 
 const Top = () => {
-  return <div>Top</div>;
+  return (
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default Top;
