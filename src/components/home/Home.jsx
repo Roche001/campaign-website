0cddb3032ed91a7cd246cc4c5397c33291f1d263
Roche001 <rocheharold13@gmail.com>
2022-06-22 12:15:08 +0300

    update changes

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 import "./Home.css";
 const Home = () => {
-  return <div id="Home" className="home"></div>;
+  return (
+    <div id="Home" className="home">
+      <div className="home-header">
+        <h2>Take Action!</h2>
+        <h5>Stop the Violence.</h5>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
