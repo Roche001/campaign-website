0c6d06e64baab316dc59dc0c856881eceb939811
Roche001 <rocheharold13@gmail.com>
2022-06-22 14:09:30 +0300

    update changes

--- src/components/wellbeing/Wellbeing.jsx
+++ src/components/wellbeing/Wellbeing.jsx
@@ -2,12 +2,35 @@ import React from "react";
 import "./Wellbeing.css";
 const Wellbeing = () => {
   return (
-    <div id="Wellbeing">
+    <div
+      id="Wellbeing"
+      className="container-fluid
+    "
+    >
       <div className="wellbeing-header">
         <h5>
           Ways to Foster Mental health Wellbeing for Victim-Survivors in the
           Community
         </h5>
+      </div>
+      <div className="well-being-text">
+        <p>
+          There are several ways to foster the mental health wellness of victims
+          and survivors of violence, including children, youths, and women, in
+          the community. Some of these ways include creating an emotional safety
+          plan, exploring survivors’ strengths, creating self-care programs,
+          looking for professional mental health support, and learning more
+          about setting personal boundaries. Seeking trauma-informed support can
+          also help, such as family partnership and mental health resources.{" "}
+        </p>
+        <div className="notes-4">
+          <h5>Note:</h5>
+          <p>
+            There are several ways to foster the mental health wellness of
+            victims and survivors of violence, including children, youths, and
+            women, in the community.{" "}
+          </p>
+        </div>
       </div>
     </div>
   );
