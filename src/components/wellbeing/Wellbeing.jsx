bfc4e8cf8414b88417fc188671a93f30152d0a2e
Roche001 <rocheharold13@gmail.com>
2022-06-22 13:53:45 +0300

    update changes

--- src/components/wellbeing/Wellbeing.jsx
+++ src/components/wellbeing/Wellbeing.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 import "./Wellbeing.css";
 const Wellbeing = () => {
-  return <div id="Wellbeing"></div>;
+  return (
+    <div id="Wellbeing">
+      <div className="wellbeing-header">
+        <h5>
+          Ways to Foster Mental health Wellbeing for Victim-Survivors in the
+          Community
+        </h5>
+      </div>
+    </div>
+  );
 };
 
 export default Wellbeing;
